49af81f2ca5a3726a3291a0aa12088ec7485424a
Abdullah <113323438+Dementor28@users.noreply.github.com>
2024-11-21 16:25:15 -0500

    Add intro section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import { meal } from "../../constants";
+import "./Intro.css";
 
-import './Intro.css';
+const Intro = () => {
+  const vidRef = React.useRef();
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+  return (
+    <div className="app__bg app__wrapper section__padding">
+    <div className="app__video">
+      <video
+        ref={vidRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+        autoPlay
+      />
+    </div>
+    </div>
+  );
+};
 
 export default Intro;
