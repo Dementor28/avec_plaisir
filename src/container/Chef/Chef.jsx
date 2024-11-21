309418dc3acf87642cbeddbfb32cc32f8e09a0be
Abdullah <113323438+Dementor28@users.noreply.github.com>
2024-11-21 16:02:14 -0500

    Added Chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">Our Culinary Philosophy</h1>
+
+      <div className="app__chef-content">
+  <div className="app__chef-content_quote">
+    <img src={images.quote} alt="quote_image" />
+    <p className="p__opensans">At the heart of our kitchen lies a passion for crafting unforgettable flavors and experiences."</p>
+  </div>
+  <p className="p__opensans">
+    Every dish tells a story, blending tradition with innovation. We believe in using the finest ingredients, embracing sustainable practices, 
+    and delivering meals that nourish both body and soul.
+  </p>
+</div>
+
+
+      <div className="app__chef-sign">
+        <p>Abdullah</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
   </div>
 );
 
